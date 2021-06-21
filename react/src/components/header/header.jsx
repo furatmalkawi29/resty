34f4fd46e039584d6a6e01821e151734ddf456ea
furatmalkawi29 <malkawifurat29@gmail.com>
2021-06-21 03:31:02 +0300

    done app

---
+++ react/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../design/design.scss'
+import './header.scss'
+
+
+function Header () {
+    return (
+        <header>
+          <h1>RESTy</h1>
+        </header>)
+}
+
+export default Header;
