62297901045a0457cc60d360d94199dea4d8d0b6
furatmalkawi29 <malkawifurat29@gmail.com>
2021-07-01 23:19:40 +0300

    ok

--- src/views/help.jsx
+++ src/views/help.jsx
@@ -5,8 +5,17 @@ export class Help extends Component {
     return (
       
       <section className="results-sec">
-          App Documentation
-        </section>
+      <div>- You are able to use all REST methods GET divOST DELETE PUT</div> 
+     <div>add your url in the sdivace and hit GO!</div>
+     
+     <div>In Home page you will click on any previous route and it will be populated in form again</div>
+     <div>You will get results in home page on the side</div>
+     <div>In History page you will get pre saved results from before</div>
+     <div> You can click on routes to get results</div>
+     <div> You can click on re-run to get the route populated again in form at home page</div>
+     
+     
+             </section>
       
     )
   }
